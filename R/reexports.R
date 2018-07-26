47c7b7ad833ea40cd2561c71c58ea679ffbabb10
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2018-07-26 23:39:45 +0200

    reexporting functions from other package

---
+++ R/reexports.R
@@ -0,0 +1,16 @@
+#' Objects exported from other packages
+#'
+#' @rdname reexports
+#'
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
+#' @importFrom dplyr mutate
+#' @export
+dplyr::mutate
+#' @importFrom dplyr filter
+#' @export
+dplyr::filter
+#' @importFrom dplyr select
+#' @export
+dplyr::select
