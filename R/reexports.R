6aa3bd8dcd47d908aed7ac5ee48c37789e3cd61b
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2018-08-11 12:11:27 +0200

    dplyr filter reexported

--- R/reexports.R
+++ R/reexports.R
@@ -4,6 +4,9 @@ magrittr::`%>%`
 #' @importFrom dplyr mutate
 #' @export
 dplyr::mutate
+#' @importFrom dplyr filter
+#' @export
+dplyr::filter
 #' @importFrom dplyr group_by
 #' @export
 dplyr::group_by
