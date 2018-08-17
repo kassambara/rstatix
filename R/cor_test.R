d297fea1d71166e80eefa6570fd8e6197873720b
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2018-08-17 21:33:35 +0200

    R code simplified

--- R/cor_test.R
+++ R/cor_test.R
@@ -10,20 +10,27 @@ NULL
 #'  between paired samples, using Pearson, Kendall or Spearman method. Wrapper
 #'  around the function \code{\link[stats]{cor.test}()}.
 #'
-#'  Available functions include: \itemize{ \item \code{cor_test()}: Correlation
-#'  test between two or variables. \item \code{mcor_test()}: Multiple
-#'  correlation tests between two vectors of variables. Using this function, you
-#'  can compute, for example, the correlation between one variable vs many. }
+#'  Can also performs multiple pairwise correlation analyses between more than
+#'  two variables or between two different vectors of variables. Using this function, you
+#'  can also compute, for example, the correlation between one variable vs many.
 #'
 #'
 #'@inheritParams stats::cor.test
 #'@inheritParams stats::cor
 #'@param data a data.frame containing the variables.
-#'@param vars a character vector containing at least two variable names. If
-#'  NULL, multiple pairwise correlation tests is performed between all variables
-#'  in the data.
-#'@param x,y character vectors containing variable names to be used in the
-#'  correlation analysis.
+#'@param vars a character vector containing variable names for correlation
+#'  analysis. \itemize{ \item If \code{vars} is NULL, multiple pairwise
+#'  correlation tests is performed between all variables in the data. \item If
+#'  \code{vars} contain only one variable, a pairwise correlation analysis is
+#'  performed between the specified variable vs either all the remaining numeric
+#'  variables in the data or variables in \code{vars2} (if specified). \item If
+#'  \code{vars} contain two or more variables: i) if \code{vars2} is not
+#'  specified, a pairwise correlation analysis is performed between all possible
+#'  combinations of variables. ii) if \code{vars2} is specified, each element in
+#'  \code{vars} is tested against all elements in \code{vars2}}
+#'@param vars2 optional character vector. If specified, each element in
+#'  \code{vars} is tested against all elements in \code{vars2}.
+#'
 #'@param ... other arguments to be passed to the function \code{cor_test()} or
 #'  \code{\link[stats]{cor.test}()}.
 #'
@@ -33,7 +40,14 @@ NULL
 #'  statistic used to compute the p-value. \item \code{p}: p-value. \item
 #'  \code{conf.low,conf.high}: Lower and upper bounds on a confidende interval.
 #'  \item \code{method}: the method used to compute the statistic.}
+#' @seealso cor_mat
 #' @examples
+#'
+#' # Correlation between the specified variable vs
+#' # the remaining numeric variables in the data
+#' #:::::::::::::::::::::::::::::::::::::::::
+#' mtcars %>% cor_test("mpg")
+#'
 #' # Correlation test between two variables
 #' #:::::::::::::::::::::::::::::::::::::::::
 #' mtcars %>% cor_test(c("wt", "mpg"))
@@ -51,65 +65,78 @@ NULL
 #' # Multiple correlation test
 #' #:::::::::::::::::::::::::::::::::::::::::
 #' # Correlation between one variable vs many
-#' mtcars %>% mcor_test(x = "mpg", y = c("disp", "hp", "drat"))
+#' mtcars %>% cor_test(
+#'   vars = "mpg",
+#'   vars2 = c("disp", "hp", "drat")
+#'  )
 #'
 #' # Correlation between two vectors of variables
-#' mtcars %>% mcor_test(x = c("mpg", "wt"), y = c("disp", "hp", "drat"))
+#' # Each element in vars is tested against all elements in vars2
+#' mtcars %>% cor_test(
+#'   vars = c("mpg", "wt"),
+#'   vars2 = c("disp", "hp", "drat")
+#'  )
 #'
 #'
 #'@describeIn cor_test Correlation test between two variables.
 #'@export
 
 cor_test <- function(
-  data, vars = NULL, alternative = "two.sided",
+  data, vars = NULL, vars2 =  NULL, alternative = "two.sided",
   method = "pearson", conf.level = 0.95,
   use = "pairwise.complete.obs", ...
 )
 {
 
   n.vars <- length(vars)
+  # Select only numeric columns
+  data <- data %>%
+    dplyr::select_if(is.numeric)
 
-  # Pairwise correlation test between all vars in data
-  if(is.null(vars)){
-    # Select only numeric columns
-    data <- data %>% dplyr::select_if(is.numeric)
-    # Multiple correlation tests
-    vars <- colnames(data)
-    res <- mcor_test(
-      data, x = vars, y = vars,
-      alternative = alternative, method = method,
-      conf.level = conf.level, use = use, ...
-    )
+  if(is.null(vars2)){
+
+    if(is.null(vars)){
+      # Pairwise correlation test between all vars in data
+      vars <- vars2 <- colnames(data)
+    }
+    else if(n.vars == 1){
+      # Correlation between the specified variable vs
+      # all numeric vars in the data
+      vars2 <- colnames(data) %>% setdiff(vars)
+    }
+    else if(n.vars == 2){
+      # Correlation test between two variables
+      vars2 <- vars[2]
+      vars <- vars[1]
+    }
+    # Multiple pairwise correlation between multiple variables
+    else if(n.vars >2){
+      vars2 <- vars
+    }
+  }
+  else if(is.null(vars)){
+    stop("You should specify the argument vars in addition to vars2")
   }
 
-  # Correlation test between two variables
-  else if(n.vars == 2){
-    res <- cor_test_xy(
-      data, x = vars[1], y = vars[2],
-      alternative = alternative, method = method,
-      conf.level = conf.level, use = use, ...
-    )
-  }
-
-  # Multiple pairwise correlation between multiple variables
-  else if(n.vars >2){
-    res <- mcor_test(
-      data, x = vars, y = vars,
-      alternative = alternative, method = method,
-      conf.level = conf.level, use = use, ...
-    )
-  }
-
-  else{
-    stop("At least, two varables needed")
-  }
-
+  res <- mcor_test(
+    data, x = vars, y = vars2,
+    alternative = alternative, method = method,
+    conf.level = conf.level, use = use, ...
+  )
   structure(res, class = c(class(res), "cor_test") )
 }
 
 
-#' @describeIn cor_test Multiple correlation tests between two vectors of variables.
-#' @export
+
+
+# Helper functions
+#:::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+# Multiple correlation tests between two vectors of variables.
+#++++++++++++++++++++++++++++++++++++++++++++++++++++
+# x,y character vectors containing variable names to be used in the
+# correlation analysis.
 mcor_test <- function(data, x, y, ...){
 
   variables.grid <- expand.grid(y = y, x = x,  stringsAsFactors = FALSE)
@@ -119,11 +146,8 @@ mcor_test <- function(data, x, y, ...){
 }
 
 
-
-
-# Helper functions
-#:::::::::::::::::::::::::::::::::::::::::::::::::::
-
+# Correlation test between two variables x and y
+#++++++++++++++++++++++++++++++++++++++++++++++++++++
 cor_test_xy <- function(
   data, x, y, method = "pearson",
   use = "pairwise.complete.obs", exact = FALSE,  ...
