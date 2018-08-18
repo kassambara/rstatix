eb975e50da163c3976b496391f215e8809fd3ca1
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2018-08-18 10:21:52 +0200

    new argument replacement added

--- R/pull_triangle.R
+++ R/pull_triangle.R
@@ -4,8 +4,12 @@ NULL
 #' @description Returns the lower or the upper triangular part of a
 #'   (correlation) matrix.
 #' @param x a (correlation) matrix
-#' @param diagonal logical. Default is FALSE. If TRUE, the matrix diagonal is included.
-#' @param triangle the type of triangle to pull. Allowed values are one of "upper" and "lower".
+#' @param diagonal logical. Default is FALSE. If TRUE, the matrix diagonal is
+#'   included.
+#' @param triangle the type of triangle to pull. Allowed values are one of
+#'   "upper" and "lower".
+#' @param replacement Appropriate values are either "" or NA. Used to replace
+#'   the upper, lower or the diagonal part of the matrix.
 #' @return a data frame
 #' @examples
 #' # Compute correlation matrix
@@ -21,38 +25,39 @@ NULL
 
 #' @describeIn pull_triangle Returns either the lower or upper triangular part of a matrix.
 #' @export
-pull_triangle <- function(x, triangle = c("lower", "upper"), diagonal = FALSE){
+pull_triangle <- function(x, triangle = c("lower", "upper"),
+                          diagonal = FALSE, replacement = ""){
 
   triangle <-match.arg(triangle)
   switch(
     triangle,
-    lower = pull_lower_triangle(x, diagonal = diagonal),
-    upper = pull_upper_triangle(x, diagonal = diagonal)
+    lower = pull_lower_triangle(x, diagonal = diagonal, replacement = replacement),
+    upper = pull_upper_triangle(x, diagonal = diagonal, replacement = replacement)
   )
 }
 
 #' @describeIn pull_triangle Returns the upper triangular part of a matrix.
 #' @export
-pull_upper_triangle <- function(x, diagonal = FALSE){
+pull_upper_triangle <- function(x, diagonal = FALSE, replacement = ""){
 
   if(inherits(x, "tbl_df"))
     x <- .tibble_to_matrix(x)
 
-  x[lower.tri(x)] <- ""
-  if (!diagonal) diag(x) <- ""
+  x[lower.tri(x)] <- replacement
+  if (!diagonal) diag(x) <- replacement
 
   .matrix_to_dataframe(x)
 }
 
 #' @describeIn pull_triangle Returns the lower triangular part of a matrix.
 #' @export
-pull_lower_triangle <- function(x, diagonal = FALSE){
+pull_lower_triangle <- function(x, diagonal = FALSE, replacement = ""){
 
   if(inherits(x, "tbl_df"))
     x <- .tibble_to_matrix(x)
 
-  x[upper.tri(x)] <- ""
-  if (!diagonal) diag(x) <- ""
+  x[upper.tri(x)] <- replacement
+  if (!diagonal) diag(x) <- replacement
   .matrix_to_dataframe(x)
 }
 
