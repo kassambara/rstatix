ce9fd4c217d18bb913aa1f0f335e8ec4447d281a
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2019-04-29 07:39:55 +0200

    New function get_mode() added

---
+++ R/get_mode.R
@@ -0,0 +1,42 @@
+#' @include utilities.R
+NULL
+#' Compute Mode
+#'
+#' @description Compute the mode in a given vector. Mode is the most frequent
+#'   value.
+#'
+#' @param x a vector. Can be numeric, factor or character vetor.
+#'
+#' @examples
+#'
+#' # Mode of numeric vector
+#' x <- c(1:5, 6, 6, 7:10)
+#' get_mode(x)
+#'
+#' # Bimodal
+#' x <- c(1:5, 6, 6, 7, 8, 9, 9, 10)
+#' get_mode(x)
+#'
+#' # No mode
+#' x <- c(1, 2, 3, 4, 5)
+#' get_mode(x)
+#'
+#' # Nominal vector
+#' fruits <-  c(rep("orange", 10), rep("apple", 5), rep("lemon", 2))
+#' get_mode(fruits)
+#' @export
+get_mode <- function(x){
+  .x <- factor(x)
+  .table <- table(.x)
+  .max <- max(.table)
+  if(all(.table == .max)){
+    .mode <- NA
+  }
+  else{
+   .mode <- names(.table)[.table == .max]
+  }
+  if(is.numeric(x)){
+    .mode <- as.numeric(.mode)
+  }
+  .mode
+}
