81b971f117e110b17888a9a9025035ccfdd03d21
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2019-05-19 21:14:43 +0200

    quantile option added

--- R/get_summary_stats.R
+++ R/get_summary_stats.R
@@ -9,10 +9,11 @@ NULL
 #'  data frame is computed.
 #'@param type type of summary statistics. Possible values include: \code{"full",
 #'  "common", "robust",  "five_number", "mean_sd", "mean_se", "mean_ci",
-#'  "median_iqr", "median_mad"}
+#'  "median_iqr", "median_mad", "quantile"}
 #'@param show a character vector specifying the summary statistics you want to
 #'  show. Example: \code{show = c("n", "mean", "sd")}. This is used to filter
 #'  the output after computation.
+#' @param probs numeric vector of probabilities with values in [0,1]. Used only when type = "quantile".
 #'@return A data frame containing descriptive statistics, such as: \itemize{
 #'  \item \strong{n}: the number of individuals \item \strong{min}: minimum
 #'  \item \strong{max}: maximum \item \strong{median}: median \item
@@ -47,8 +48,8 @@ NULL
 #'@export
 get_summary_stats <- function(
   data, ..., type = c("full", "common", "robust",  "five_number",
-                      "mean_sd", "mean_se", "mean_ci", "median_iqr", "median_mad"),
-  show = NULL
+                      "mean_sd", "mean_se", "mean_ci", "median_iqr", "median_mad", "quantile"),
+  show = NULL, probs = seq(0, 1, 0.25)
   ){
   type = match.arg(type)
   if(is_grouped_df(data)){
@@ -77,6 +78,7 @@ get_summary_stats <- function(
     mean_ci = mean_ci(data),
     median_iqr = median_iqr(data),
     median_mad = median_mad(data),
+    quantile = quantile_summary(data, probs),
     full_summary(data)
   ) %>%
     dplyr::mutate_if(is.numeric, round, digits = 3)
@@ -142,6 +144,25 @@ robust_summary <- function(data){
       iqr = stats::IQR(value, na.rm=TRUE)
     )
 }
+
+quantile_summary <- function(data, probs = seq(0, 1, 0.25)){
+  core_func <- function(data, probs){
+    value <- NULL
+    n <- sum(!is.na(data$value))
+    names(n) <- "n"
+    q <- stats::quantile(data$value, probs, na.rm = TRUE)
+    results <- t(matrix(c(n, q)))
+    colnames(results) <- c("n", names(q))
+    tibble::as_tibble(results)
+  }
+  results  <- data %>%
+    nest() %>%
+    mutate(.results. = map(data, core_func, probs)) %>%
+    select(.data$variable, .data$.results.) %>%
+    unnest()
+  results
+}
+
 
 
 five_number_summary <- function(data){
