85e9f103ef3127ab1bb8f8e8067d20da8b2e2a86
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2020-07-22 01:11:32 +0200

    Test added for grouped t-test

--- tests/testthat/test-get_n.R
+++ tests/testthat/test-get_n.R
@@ -6,6 +6,14 @@ test_that("Checking that get_n works for T-test", {
   expect_equal(get_n(stat.test), c(40, 40, 40))
 })
 
+test_that("Checking that get_n works for grouped T-test", {
+  data("ToothGrowth")
+  stat.test <- ToothGrowth %>%
+    group_by(dose) %>%
+    t_test(len ~ supp)
+  expect_equal(get_n(stat.test), c(20, 20, 20))
+})
+
 test_that("Checking that get_n works for grouped ANOVA", {
   data("ToothGrowth")
   res.aov <- ToothGrowth %>%
