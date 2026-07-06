223fcbedc3bc15132c01376b341a11bd733073c3
Hannes <hoberreiter@gmail.com>
2026-07-06 10:29:27 +0200

    Add bootstrap parallel options

--- R/utilities.R
+++ R/utilities.R
@@ -798,10 +798,11 @@ get_pairwise_comparison_methods <- function(){
 }
 
 # Bootstrap confidence intervals -------------------------
-get_boot_ci <- function(data, stat.func, conf.level = 0.95, type = "perc", nboot = 500){
+get_boot_ci <- function(data, stat.func, conf.level = 0.95, type = "perc", nboot = 500,
+                        parallel = "no", ncpus = getOption("boot.ncpus", 1L)){
   required_package("boot")
-  Boot = boot::boot(data, stat.func, R = nboot)
-  BCI = boot::boot.ci(Boot, conf = conf.level, type = type, parallel = "multicore")
+  Boot = boot::boot(data, stat.func, R = nboot, parallel = parallel, ncpus = ncpus)
+  BCI = boot::boot.ci(Boot, conf = conf.level, type = type)
   type <- switch(
     type, norm = "normal", perc = "percent",
     basic = "basic", bca = "bca", stud = "student", type
